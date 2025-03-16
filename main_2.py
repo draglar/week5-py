cfb891c54fb8df8ddf42f04adc564c1629281a66
draglar <ianngash41@gmail.com>
2025-03-16 17:31:52 +0300

    doe

--- main_2.py
+++ main_2.py
@@ -3,3 +3,25 @@ Activity 2: Polymorphism Challenge! 🎭
 
 Create a program that includes animals or vehicles with the same action (like move()). However, make each class define move() differently (for example, Car.move() prints "Driving" 🚗, while Plane.move() prints "Flying" ✈️).
 '''
+class Cat:
+    def __init__(self) -> None:
+        self.movement = 'Walking'
+        pass
+    def move(self):
+        return self.movement
+
+class Fish:
+    def __init__(self) -> None:
+        self.movement = 'Flying'
+        pass
+    def move(self):
+        return self.movement
+class Bird:
+    def __init__(self) -> None:
+        self.movement = 'Flying'
+        pass
+    def move(self):
+        return self.movement
+
+for animal in[Cat(),Fish(),Bird()]:
+    print('movement by ',animal.move())
